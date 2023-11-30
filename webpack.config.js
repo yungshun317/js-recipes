7287485575c6a14f2865ba1672aca6cf17429828
yungshun317 <seraph317@gmail.com>
2023-11-30 11:04:26 +0800

    [CHORE] Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+
+const config = {
+    mode: "development",
+    entry: [
+        __dirname + "./src/js/controller.js",
+        __dirname + "./src/sass/main.scss"
+    ],
+    output: {
+        path: path.resolve(__dirname, 'src'),
+        filename: 'bundled.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules)/,
+                use: []
+            }, {
+                test: /\.(s(a|c)ss)$/,
+                // use: ['style-loader','css-loader', 'sass-loader']
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: { outputPath: "src/", name: "style.css" }
+                    },
+                    "sass-loader"
+                ]
+            }
+        ]
+    },
+}
+
+module.exports = config;
