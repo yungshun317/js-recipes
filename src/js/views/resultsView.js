79a2c2a35ab665bed2481cc45a9c057e497e1724
yungshun317 <seraph317@gmail.com>
2023-12-12 13:16:05 +0800

    [FEAT] Generate the preview

--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -5,15 +5,19 @@ class ResultsView extends View {
 
     _generateMarkup() {
         console.log(this._data);
+        return this._data.map(this.#generateMarkupPreview).join('');
+    }
+
+    #generateMarkupPreview(result) {
         return  `
             <li class="preview">
-              <a class="preview__link preview__link--active" href="#23456">
+              <a class="preview__link preview__link--active" href="#${result.id}">
                 <figure class="preview__fig">
-                  <img src="src/img/test-1.jpg" alt="Test" />
+                  <img src="${result.image}" alt="Test" />
                 </figure>
                 <div class="preview__data">
-                  <h4 class="preview__title">Pasta with Tomato Cream ...</h4>
-                  <p class="preview__publisher">The Pioneer Woman</p>
+                  <h4 class="preview__title">${result.title}</h4>
+                  <p class="preview__publisher">${result.publisher}</p>
                   <div class="preview__user-generated">
                     <svg>
                       <use href="src/img/icons.svg#icon-user"></use>
@@ -24,6 +28,7 @@ class ResultsView extends View {
             </li>
         `;
     }
+
 }
 
 export default new ResultsView();
