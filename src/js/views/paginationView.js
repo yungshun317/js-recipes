857fe3ffb621757b9ec08e549cdeffc816d61504
yungshun317 <seraph317@gmail.com>
2023-12-16 14:13:23 +0800

    [FEAT] Render the pagination buttons

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -4,26 +4,54 @@ class PaginationView extends View {
     _parentElement = document.querySelector('.pagination');
 
     _generateMarkup() {
+        const curPage = this._data.page;
         const numPages = Math.ceil(this._data.results.length / this._data.resultsPerPage);
         console.log(numPages);
 
         // Page 1, and there are other pages
-        if (this._data.page === 1 && numPages > 1) {
-            return 'page 1, others';
+        if (curPage === 1 && numPages > 1) {
+            return `
+                <button class="btn--inline pagination__btn--next">
+                  <span>Page ${curPage + 1}</span>
+                  <svg class="search__icon">
+                    <use href="src/img/icons.svg#icon-arrow-right"></use>
+                  </svg>
+                </button>
+            `;
         }
 
         // Last page
-        if (this._data.page === numPages && numPages > 1) {
-            return 'last page';
+        if (curPage === numPages && numPages > 1) {
+            return `
+                <button class="btn--inline pagination__btn--prev">
+                  <svg class="search__icon">
+                    <use href="src/img/icons.svg#icon-arrow-left"></use>
+                  </svg>
+                  <span>Page ${curPage - 1}</span>
+                </button>
+            `;
         }
 
         // Other page
-        if (this._data.page < numPages) {
-            return 'other page';
+        if (curPage < numPages) {
+            return `
+                <button class="btn--inline pagination__btn--prev">
+                  <svg class="search__icon">
+                    <use href="src/img/icons.svg#icon-arrow-left"></use>
+                  </svg>
+                  <span>Page ${curPage - 1}</span>
+                </button>
+                <button class="btn--inline pagination__btn--next">
+                  <span>Page ${curPage + 1}</span>
+                  <svg class="search__icon">
+                    <use href="src/img/icons.svg#icon-arrow-right"></use>
+                  </svg>
+                </button>
+            `;
         }
 
         // Page 1, and there are no other pages
-        return 'only 1 page';
+        return '';
     }
 }
 
