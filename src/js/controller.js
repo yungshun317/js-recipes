5f86608b6e5a4e78e8a65e112694b1c4cf8877ca
yungshun317 <seraph317@gmail.com>
2023-12-22 18:20:22 +0800

    [FEAT] Create the `BookmarksView` class

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,6 +3,7 @@ import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
+import bookmarksView from './views/bookmarksView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -73,12 +74,18 @@ const controlServings = function (newServings) {
 }
 
 const controlAddBookmark = function () {
+    // [1] Add/remove bookmark
     if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
     else model.deleteBookmark(model.state.recipe.id);
 
-    console.log(model.state.recipe);
+    // console.log(model.state.recipe);
     // bookmarked: true
+
+    // [2] Update recipe view
     recipeView.update(model.state.recipe);
+
+    // [3] Render bookmarks
+    bookmarksView.render(model.state.bookmarks);
 }
 
 const init = function() {
