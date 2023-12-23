fda78082e26c20f228c5f125e9f4fb34318724f7
yungshun317 <seraph317@gmail.com>
2023-12-23 18:59:50 +0800

    [FEAT] Persist bookmarks with `localStorage`

--- src/js/model.js
+++ src/js/model.js
@@ -82,12 +82,18 @@ export const updateServings = function (newServings) {
     state.recipe.servings = newServings;
 }
 
+const persistBookmarks = function () {
+    localStorage.setItem('bookmarks', JSON.stringify(state.bookmarks));
+}
+
 export const addBookmark = function (recipe) {
     // Add bookmark
     state.bookmarks.push(recipe);
 
     // Mark current recipe as bookmark
     if (recipe.id === state.recipe.id) state.recipe.bookmarked = true;
+
+    persistBookmarks();
 }
 
 export const deleteBookmark = function (id) {
@@ -97,4 +103,6 @@ export const deleteBookmark = function (id) {
 
     // Mark current recipe as not bookmarked
     if (id === state.recipe.id) state.recipe.bookmarked = false;
+
+    persistBookmarks();
 }
