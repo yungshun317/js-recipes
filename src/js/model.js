fcffb0a999ffa5fa551d359cbbf6475b4f783215
yungshun317 <seraph317@gmail.com>
2023-12-30 00:05:29 +0800

    [FEAT] Display the user icons

--- src/js/model.js
+++ src/js/model.js
@@ -32,7 +32,7 @@ const createRecipeObject = function (data) {
 
 export const loadRecipe = async function (id) {
     try {
-        const data = await AJAX(`${API_URL}/${id}`);
+        const data = await AJAX(`${API_URL}${id}?key=${KEY}`);
         state.recipe = createRecipeObject(data);
 
         if (state.bookmarks.some(bookmark => bookmark.id === id)) state.recipe.bookmarked = true;
@@ -50,7 +50,7 @@ export const loadSearchResults = async function (query) {
     try {
         state.search.query = query;
 
-        const data = await AJAX(`${API_URL}?search=${query}`);
+        const data = await AJAX(`${API_URL}?search=${query}&key=${KEY}`);
         console.log(data);
 
         state.search.results = data.data.recipes.map(rec => {
@@ -58,7 +58,8 @@ export const loadSearchResults = async function (query) {
                id: rec.id,
                title: rec.title,
                publisher: rec.publisher,
-               image: rec.image_url
+               image: rec.image_url,
+               ...(rec.key && { key: rec.key })
            }
         });
         // Whenever do a new search, the page will reset to 1
@@ -125,7 +126,7 @@ const clearBookmarks = function () {
     localStorage.clear('bookmarks');
 }
 
-// clearBookmarks();
+clearBookmarks();
 
 export const uploadRecipe = async function (newRecipe) {
     try {
